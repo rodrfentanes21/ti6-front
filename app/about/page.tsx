54642af78e0b7be40f5bb4bb59dfad863d43b513
Rodrigo <rodrigo.fentanes@sofex.com.br>
2023-09-27 14:35:09 -0300

    divided the about scections into components

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,29 @@
+import AboutComponent from '@/components/AboutPage/AboutComponent';
+
 export default function Home() {
-	return <div className='text-underline'>about</div>;
+	return (
+		<main className='w-screen lg:w-full flex flex-col justify-center items-center gap-10'>
+			<section className='w-full h-screen flex justify-center items-center'>
+				<div className='text-transparent bg-clip-text bg-gradient-to-r from-emerald-600 to-emerald-900 text-6xl h-48 grid place-items-center'>
+					O Projeto
+				</div>
+			</section>
+			<AboutComponent
+				title='Teste'
+				body='Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eveniet at praesentium deserunt suscipit cupiditate, aliquid alias voluptatum aspernatur ut, quaerat, minus obcaecati vel nobis voluptatibus ipsa facere possimus libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolor ab corporis odio aperiam veritatis est eaque, architecto magni ipsa culpa esse eius libero labore! Nulla sapiente dolor mollitia eveniet! Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eveniet at praesentium deserunt suscipit cupiditate, aliquid alias voluptatum aspernatur ut, quaerat, minus obcaecati vel nobis voluptatibus ipsa facere possimus libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolor ab corporis odio aperiam veritatis est eaque, architecto magni ipsa culpa esse eius libero labore! Nulla sapiente dolor mollitia eveniet!'
+				// add image props later
+			/>
+			<AboutComponent
+				title='Teste'
+				body='Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eveniet at praesentium deserunt suscipit cupiditate, aliquid alias voluptatum aspernatur ut, quaerat, minus obcaecati vel nobis voluptatibus ipsa facere possimus libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolor ab corporis odio aperiam veritatis est eaque, architecto magni ipsa culpa esse eius libero labore! Nulla sapiente dolor mollitia eveniet! Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eveniet at praesentium deserunt suscipit cupiditate, aliquid alias voluptatum aspernatur ut, quaerat, minus obcaecati vel nobis voluptatibus ipsa facere possimus libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolor ab corporis odio aperiam veritatis est eaque, architecto magni ipsa culpa esse eius libero labore! Nulla sapiente dolor mollitia eveniet!'
+				// add image props later
+				reversed
+			/>
+			<AboutComponent
+				title='Teste'
+				body='Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eveniet at praesentium deserunt suscipit cupiditate, aliquid alias voluptatum aspernatur ut, quaerat, minus obcaecati vel nobis voluptatibus ipsa facere possimus libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolor ab corporis odio aperiam veritatis est eaque, architecto magni ipsa culpa esse eius libero labore! Nulla sapiente dolor mollitia eveniet! Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eveniet at praesentium deserunt suscipit cupiditate, aliquid alias voluptatum aspernatur ut, quaerat, minus obcaecati vel nobis voluptatibus ipsa facere possimus libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolor ab corporis odio aperiam veritatis est eaque, architecto magni ipsa culpa esse eius libero labore! Nulla sapiente dolor mollitia eveniet!'
+				// add image props later
+			/>
+		</main>
+	);
 }
---
+++ components/AboutPage/AboutComponent.tsx
@@ -0,0 +1,27 @@
+interface AboutComponentProps {
+	title: string;
+	body: string;
+	// add image later
+	reversed?: boolean;
+}
+
+function AboutComponent({ title, body, reversed }: AboutComponentProps) {
+	const flexDirection = reversed ? 'row-reverse' : 'row';
+	return (
+		<section
+			className={`flex flex-col lg:flex-${flexDirection} justify-start h-screen w-[90%] items-start py-10`}>
+			<div className='h-full w-full lg:w-1/2 flex flex-col gap-5'>
+				<h2 className=' text-dodgerblue-100 text-2xl md:text-3xl lg:text-6xl font-semibold flex justify-start items-center'>
+					{title}
+				</h2>
+				<p className='text-xs md:text-sm lg:text-base'>{body}</p>
+			</div>
+			<div className='h-full w-screen lg:w-1/2 grid place-items-center text-2x1'>
+				{/* <Image src={image} alt="Descrição da imagem" width={250} height={250} />*/}
+				teste
+			</div>
+		</section>
+	);
+}
+
+export default AboutComponent;
