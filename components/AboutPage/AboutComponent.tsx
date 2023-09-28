b5dd82a99de7da0dd9ba225b4eb37f6848a3ed43
Rodrigo <rodrigo.fentanes@sofex.com.br>
2023-09-28 17:39:32 -0300

    font weightt in aboutcomponent

--- components/AboutPage/AboutComponent.tsx
+++ components/AboutPage/AboutComponent.tsx
@@ -12,7 +12,7 @@ function AboutComponent({ title, body, image, reversed }: AboutComponentProps) {
 		<section
 			className={`flex flex-col lg:flex-row lg:even:flex-row-reverse justify-start h-screen w-[90%] items-start py-10`}>
 			<div className='h-full w-full lg:w-1/2 flex flex-col gap-5'>
-				<h2 className='text-transparent bg-clip-text bg-gradient-to-r from-emerald-600 to-emerald-900 text-2xl md:text-3xl lg:text-6xl font-semibold flex justify-start items-center'>
+				<h2 className='text-transparent bg-clip-text bg-gradient-to-r from-emerald-600 to-emerald-900 text-2xl md:text-3xl lg:text-6xl font-light flex justify-start items-center'>
 					{title}
 				</h2>
 				<p className='text-xs md:text-sm lg:text-base'>{body}</p>
