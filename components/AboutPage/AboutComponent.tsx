729d041bfc81cd5075342f88bd346a79f2b4df54
Rodrigo <rodrigo.fentanes@sofex.com.br>
2023-09-28 17:37:38 -0300

    fixed flex direction on aboutcomponent

--- components/AboutPage/AboutComponent.tsx
+++ components/AboutPage/AboutComponent.tsx
@@ -8,10 +8,9 @@ interface AboutComponentProps {
 }
 
 function AboutComponent({ title, body, image, reversed }: AboutComponentProps) {
-	const flexDirection = reversed ? 'row-reverse' : 'row';
 	return (
 		<section
-			className={`flex flex-col lg:flex-${flexDirection} justify-start h-screen w-[90%] items-start py-10`}>
+			className={`flex flex-col lg:flex-row lg:even:flex-row-reverse justify-start h-screen w-[90%] items-start py-10`}>
 			<div className='h-full w-full lg:w-1/2 flex flex-col gap-5'>
 				<h2 className='text-transparent bg-clip-text bg-gradient-to-r from-emerald-600 to-emerald-900 text-2xl md:text-3xl lg:text-6xl font-semibold flex justify-start items-center'>
 					{title}
